4b465d7bb0f7350efae5ea8b17ef555bb3b299a7
tom caruso <carusot42@gmail.com>
2017-09-07 21:58:27 -0400

    remove some commented out code

--- nugscraper/main.py
+++ nugscraper/main.py
@@ -39,10 +39,10 @@ def process(login_info, url, dest_dir, num_procs):
         click.echo("Unable to connect to nugs.net. Please check your login credentials.")
         exit(1)
 
-    scraped_songs = scrape_url(session, url)
+    scraped_songs = []
+    [scraped_songs.extend(scrape_url(session, u)) for u in url]
 
-    song_info = [(url, os.path.join(dest_dir,filename), idx) for idx, (url, filename) in enumerate(scraped_songs)]
-    # song_info = [(url, path, idx) for idx, (url, path) in enumerate(zip(links, paths))]
+    song_info = [(url, os.path.join(dest_dir, filename), idx) for idx, (url, filename) in enumerate(scraped_songs)]
 
     pool = ThreadPool(num_procs)
     lock = Lock()
@@ -51,60 +51,6 @@ def process(login_info, url, dest_dir, num_procs):
         pool.apply_async(download_file_with_progressbar, args=(url, path, idx, lock))
     pool.close()
     pool.join()
-
-
-def scratch():
-    pass
-    # links = ['http://ipv4.download.thinkbroadband.com/5MB.zip',
-    #          'http://ipv4.download.thinkbroadband.com/20MB.zip',
-    #          'http://ipv4.download.thinkbroadband.com/5MB.zip',
-    #          'http://ipv4.download.thinkbroadband.com/20MB.zip',
-    #          'http://ipv4.download.thinkbroadband.com/5MB.zip'
-    #          ]
-    # paths = [
-    #     '/tmp/5mb1',
-    #     '/tmp/20mb1',
-    #     '/tmp/5mb2',
-    #     '/tmp/20mb2',
-    #     '/tmp/5mb3',
-    # ]
-    # def main():
-    #
-    #     all_songs = []
-    #
-    #     for a in soup.findAll('a'):
-    #         match = song_title.findall(str(a))
-    #         if match:
-    #             title = match[0][3]
-    #             date = match[0][0]
-    #             disc_num = match[0][1]
-    #             song_num = match[0][2]
-    #             date = '20' + date
-    #
-    #             title = disc_num + song_num + '_' + title
-    #
-    #             link = a.get('href')
-    #             all_songs.append(
-    #                 {
-    #                     'title' : title,
-    #                     'date'  : date,
-    #                     'link'  : link
-    #                 }
-    #             )
-    #
-    #     with open('/home/tom/Music/aqueous_links.json', 'w') as f:
-    #         json.dump(all_songs, f)
-    #
-    #     for song in all_songs:
-    #         song_folder = os.path.join('/home/tom/Music/Aqueous/', song['date'])
-    #         try:
-    #             os.mkdir(song_folder)
-    #         except:
-    #             pass
-    #         song_path = os.path.join(song_folder, song['title'])
-    #         download_file(song['link'], song_path)
-    #         print("downloaded {} to {}".format(song['title'], song_folder))
-
 
 
 if __name__ == '__main__':
